13de884cb8d5b4a1cf702cd38d82090b057d936d
Richy <strunk876@gmail.com>
2021-08-29 14:33:47 +0200

    Patched WorldBorders

--- src/main/java/de/jpx3/intave/world/border/WorldBorders.java
+++ src/main/java/de/jpx3/intave/world/border/WorldBorders.java
@@ -1,5 +1,7 @@
 package de.jpx3.intave.world.border;
 
+import de.jpx3.intave.adapter.MinecraftVersions;
+import de.jpx3.intave.reflect.patchy.PatchyLoadingInjector;
 import org.bukkit.Location;
 import org.bukkit.World;
 
@@ -7,7 +9,13 @@ public final class WorldBorders {
   private static WorldBorderAccess worldBorderAccess = new BukkitWorldBorderAccess();
 
   public static void setup() {
-    worldBorderAccess = new BukkitWorldBorderAccess();
+    if (MinecraftVersions.VER1_13_0.atOrAbove()) {
+      worldBorderAccess = new BukkitWorldBorderAccess();
+    } else {
+      ClassLoader classLoader = WorldBorders.class.getClassLoader();
+      PatchyLoadingInjector.loadUnloadedClassPatched(classLoader, "de.jpx3.intave.world.border.CarefulWorldBorderAccess");
+      worldBorderAccess = new CarefulWorldBorderAccess();
+    }
     worldBorderAccess = new CachedForwardingWorldBorderAccess(worldBorderAccess);
   }
 
