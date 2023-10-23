7aea00e08d5ca0d9e723205a1d851cdfb3efa164
Richy <strunk876@gmail.com>
2023-10-24 00:41:16 +0200

    Update 14.6.9

--- src/main/java/de/jpx3/intave/IntavePlugin.java
+++ src/main/java/de/jpx3/intave/IntavePlugin.java
@@ -198,6 +198,7 @@ public final class IntavePlugin extends JavaPlugin {
 
     InterceptorDetection.setup();
 
+
     try {
       // We need to put this here before setting up the Synchronizer
       componentLoader = new ComponentLoader(this);
